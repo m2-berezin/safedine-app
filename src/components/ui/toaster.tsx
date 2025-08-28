a8b3c7282ce52660720a3248fb017fd9c58dfcf9
Maxim Berezin <maxim2.berezin@live.uwe.ac.uk>
2025-08-28 03:22:07 +0000

    Add FAQs to MainHub
    Add FAQs to the MainHub page. This change also includes reverting toaster component changes to address build issues.

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -13,24 +13,20 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      <ol className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
-        {toasts.map(function ({ id, title, description, action, ...props }) {
-          return (
-            <li key={id}>
-              <Toast {...props}>
-                <div className="grid gap-1">
-                  {title && <ToastTitle>{title}</ToastTitle>}
-                  {description && (
-                    <ToastDescription>{description}</ToastDescription>
-                  )}
-                </div>
-                {action}
-                <ToastClose />
-              </Toast>
-            </li>
-          )
-        })}
-      </ol>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
       <ToastViewport />
     </ToastProvider>
   )
