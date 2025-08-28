83daff686f3c74b211a693947fca21db20a9c252
Maxim Berezin <maxim2.berezin@live.uwe.ac.uk>
2025-08-28 02:59:23 +0000

    Fix toaster component
    The toaster component was not displaying correctly. This commit fixes the toaster component to ensure it displays properly.

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -13,20 +13,24 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
+      <ol className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
+        {toasts.map(function ({ id, title, description, action, ...props }) {
+          return (
+            <li key={id}>
+              <Toast {...props}>
+                <div className="grid gap-1">
+                  {title && <ToastTitle>{title}</ToastTitle>}
+                  {description && (
+                    <ToastDescription>{description}</ToastDescription>
+                  )}
+                </div>
+                {action}
+                <ToastClose />
+              </Toast>
+            </li>
+          )
+        })}
+      </ol>
       <ToastViewport />
     </ToastProvider>
   )
